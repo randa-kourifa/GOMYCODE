5f8086e8fd1f005ae813d8800f5181e25d871f68
Hamza Chebbah <hamzachebbah9999@gmail.com>
2022-12-23 12:24:26 +0100

    added

--- ES6/exercices/ternary.js
+++ ES6/exercices/ternary.js
@@ -1,4 +1,4 @@
-// image you have a button with a click event
+// imagine you have a button with a click event
 //write an arrow function named handleClick(age)
 //that will change the color of the button to red if
 //the age is greater than 18 and green if the age is less than 18
