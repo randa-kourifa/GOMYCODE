ad52d014d299814ff64e1bf757dfb8dfd0081c71
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-01-06 07:57:42 +0100

    added

---
+++ quiz-project/src/questions.js
@@ -0,0 +1,66 @@
+// questions about coding and programming
+export const questions = [{
+        id: 1,
+        title: 'What is the full form of HTML?',
+        options: ['Hyper Text Markup Language', 'Hyperlinks and Text Markup Language', 'Home Tool Markup Language', 'None of the above'],
+        answer: 'Hyper Text Markup Language'
+    },
+    {
+        id: 2,
+        title: 'What is the full form of CSS?',
+        options: ['Cascading Style Sheets', 'Colorful Style Sheets', 'Computer Style Sheets', 'Creative Style Sheets'],
+        answer: 'Cascading Style Sheets'
+    },
+    {
+        id: 3,
+        title: 'What is the full form of HTTP?',
+        options: ['Hyper Text Transfer Protocol', 'Hyper Text Test Protocol', 'Hyper Test Transfer Protocol', 'Hyper Text Transfer Product'],
+        answer: 'Hyper Text Transfer Protocol'
+    },
+    {
+        id: 4,
+        title: 'What is the equivalent of class in React?',
+        options: ['class', 'className', 'class-name', 'none of the above'],
+        answer: 'className'
+    },
+    {
+        id: 5,
+        title: 'What is the JSX?',
+        options: ['JavaScript XML', 'JavaScript XML Syntax', 'JavaScript Syntax', 'None of the above'],
+        answer: 'JavaScript XML'
+    },
+    {
+        id: 6,
+        title: 'What is the full form of API?',
+        options: ['Application Programming Interface', 'Application Program Interface', 'Application Programming Interfacing', 'None of the above'],
+        answer: 'Application Programming Interface'
+
+    },
+    {
+        id: 7,
+        // questions about react
+        title: "what we put in node_modules folder?",
+        options: ["dependencies", "packages", "modules", "all of the above"],
+        answer: "all of the above"
+
+    },
+    {
+        id: 8,
+        title: "what we put in package.json file?",
+        options: ["dependencies", "packages", "information about our project", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        id: 9,
+        title: "what do useState hook do?",
+        options: ["it is used to store data", "it is used to store state", "it is used to store data and state", "none of the above"],
+        answer: "it is used to store data and state"
+
+    }, {
+        id: 10,
+        title: "what is the difference between default and named export?",
+        options: ["default export can be imported with any name", "named export can be imported with any name", "default export can be imported with any name but named export can be imported with only specific name", "none of the above"],
+        answer: "default export can be imported with any name but named export can be imported with only specific name"
+
+    }
+]
