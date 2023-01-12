209d5645972e59eaeeadbbaf92203b5a744e1c84
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-01-12 19:09:36 +0100

    added ref

--- crash-course/src/App.jsx
+++ crash-course/src/App.jsx
@@ -1,29 +1,18 @@
+import { useRef } from "react"
 import "./App.css"
-import Button from "./components/Button"
-import Container from "./components/Container"
-import Form from "./components/Form"
-import User from "./components/User"
 
 function App() {
-	const name = "react"
-	const handleClick = (evenement) => {
-		console.log(evenement)
-		alert("Button clicked")
+	const myref = useRef(null)
+
+	const getRef = () => {
+		console.log(myref)
 	}
-	const users = [
-		{ name: "John", id: 1, color: "red" },
-		{ name: "Jane", id: 2, color: "blue" },
-	]
 
 	return (
 		<>
-			<Container OurProp={name} />
-			<Form />
-			{users.map((user) => (
-				<User AttributeUser={user} />
-			))}
-
-			<Button OurPropsAsFunction={handleClick} />
+			<button onClick={getRef} ref={myref}>
+				ref
+			</button>
 		</>
 	)
 }
