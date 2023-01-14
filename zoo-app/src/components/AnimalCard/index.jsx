aaeeaec78682aa3c3bd8939be37467c8e51d86df
Randa Kourifa <randa.kourifa@gmail.com>
2023-01-14 16:43:08 +0100

    ffgf

--- zoo-app/src/components/AnimalCard/index.jsx
+++ zoo-app/src/components/AnimalCard/index.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import classes from "./index.module.css"
+import React from "react";
+import classes from "./index.module.css";
 
-function AnimalCard({ name, srcimg, description }) {
-	return (
-		<div className={classes.container}>
-			<p>{name}</p>
-			<img src={srcimg} alt="" />
-			<p>{description}</p>
-		</div>
-	)
+function AnimalCard({ animal: { name, image, description } }) {
+  return (
+    <div className={classes.container}>
+      <p>{name}</p>
+      <img src={image} alt="" />
+      <p>{description}</p>
+    </div>
+  );
 }
 
-export default AnimalCard
+export default AnimalCard;
--- zoo-app/src/pages/Home.jsx
+++ zoo-app/src/pages/Home.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from "react"
-import AnimalCard from "../components/AnimalCard"
-import AnimalForm from "../components/AnimalForm"
-import Counter from "../components/Counter"
-import animals from "../constants/animals"
-import classes from "./Home.module.css"
-
-const [isLoading, setIsLoading] = useState(false)
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AnimalCard from "../components/AnimalCard";
+import AnimalForm from "../components/AnimalForm";
+import Counter from "../components/Counter";
+import classes from "./Home.module.css";
 export default function Home() {
-	const AddAnimal = async (e) => {
-		setIsLoading(true)
-		e.preventDefault()
-		console.log(e.target)
-		console.log("adding my animal")
-		setIsLoading(false)
-	}
+  const [isLoading, setIsLoading] = useState(false);
+  const [myanimals, setmyanimals] = useState([]);
 
-	return (
-		<div>
-			<Counter />
-			<h1>The Zoo</h1>
-			<p>Add Your Animal</p>
-			<AnimalForm AddAnimal={AddAnimal} />
-			{!isLoadingSpinner && (
-				<div className={classes.animalsWrapper}>
-					{animals.map((animal, i) => (
-						<AnimalCard key={i} animal={animal} />
-					))}
-				</div>
-			)}
-		</div>
-	)
+  console.log("before the component mount");
+  const AddAnimal = async (e) => {
+    e.preventDefault(); //form
+    const CloudinaryUrl = "https://api.cloudinary.com/v1_1/down3tmkx/upload";
+    const myimage = e.target.image.files[0];
+    const formData = new FormData();
+    formData.append("file", myimage);
+    formData.append("upload_preset", "rbrquree");
+    const response = await axios.post(CloudinaryUrl, formData);
+    const newAnimal = {
+      name: e.target.name.value,
+      description: e.target.description.value,
+      image: response.data.secure_url,
+    };
+    const responseServer = await axios.post(
+      "https://zoo-api-nhvk.onrender.com/animals/add",
+      newAnimal
+    );
+    setmyanimals(...myanimals, newAnimal);
+    console.log(response.data.secure_url);
+  };
+  useEffect(() => {
+    const getAnimals = async () => {
+      const res = await axios.get("https://zoo-api-nhvk.onrender.com/animals");
+      setmyanimals(res.data);
+    };
+    getAnimals();
+  }, []);
+  return (
+    <div>
+      <Counter />
+      <h1>The Zoo</h1>
+      <p>Add Your Animal</p>
+      <AnimalForm AddAnimal={AddAnimal} />
+      {!isLoading && (
+        <div className={classes.animalsWrapper}>
+          {myanimals.map((animal, i) => (
+            <AnimalCard key={i} animal={animal} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
