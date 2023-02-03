80d249465c9e026565b328605acab20a3cf28dbd
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-02-03 02:38:08 +0100

    remove swiper

--- redux-workshop/src/components/MovieList/index.jsx
+++ redux-workshop/src/components/MovieList/index.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react"
-import MovieCard from "../MovieCard"
 
 // import required modules
 
@@ -9,15 +7,6 @@ export default function MovieList({ Movies, handleToggle }) {
 	return (
 		<>
 			<h1>Movies</h1>
-			<Swiper slidesPerView={3} className="mySwiper">
-				{Movies.map((movie) =>
-					!movie.watched ? (
-						<SwiperSlide key={movie.id}>
-							<MovieCard handleToggle={handleToggle} {...movie} />
-						</SwiperSlide>
-					) : null,
-				)}
-			</Swiper>
 		</>
 	)
 }
--- redux-workshop/src/components/MoviesToWatch/index.jsx
+++ redux-workshop/src/components/MoviesToWatch/index.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react"
-import MovieCard from "../MovieCard"
 
 // import required modules
 
@@ -9,15 +7,6 @@ export default function MoviesToWatch({ Movies, handleToggle }) {
 	return (
 		<>
 			<h1>Your list</h1>
-			<Swiper slidesPerView={3} className="mySwiper">
-				{Movies.map((movie) =>
-					movie.watched ? (
-						<SwiperSlide key={movie.id}>
-							<MovieCard {...movie} handleToggle={handleToggle} type="mylist" />
-						</SwiperSlide>
-					) : null,
-				)}
-			</Swiper>
 		</>
 	)
 }
